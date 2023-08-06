fafc61ca8d4440d952e04599f2294da15418344c
Vikram Nirwan <vnirwan642@gmail.com>
2023-08-06 22:29:28 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-import AlmaBetter from "../assets/logo.png";
-
 export const Header = () => {
   return (
     <div className="w-20 ml-5 mt-2">
-      <img src={AlmaBetter} alt="Logo" />
+      <h1 className="font-bold text-red-700">CRYPTODASH</h1>
     </div>
   );
 };
